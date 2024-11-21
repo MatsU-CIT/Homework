13e10e22c0b4af3f7f913e73c214a3925c768f33
Yusuke Matsumoto <yusuke20050315@gmail.com>
2024-11-21 20:48:59 +0900

    課題用コマンド

--- hwcode.py
+++ hwcode.py
@@ -2,3 +2,4 @@
 # SPDX-FileCopyrightText: 2024 Yusuke Matsumoto s23c1134bg@s.chibakoudai.jp
 test = ["G", "I", "T", "H", "U", "B" ]
 print("Kokowa" , test[0:6])
+print( "Everybody ", test[5], test[4], test[0:2], test[4], test[0:2])
