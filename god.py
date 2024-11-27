82bba4f77fd7d362aa90cf497f77115b5331ed70
Yusuke Matsumoto <yusuke20050315@gmail.com>
2024-11-27 10:11:27 +0900

    お告げ実行用

---
+++ god.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Yusuke Matsumoto s23c1134bg@s.chibakoudai.jp
+
+filename = "godsmsg.txt"
+
+try:
+
+    with open(filename, "r", encoding="utf-8") as file:
+        print(f"----------神は言っている----------")
+        content = file.read()
+        print(content)
+        print("---------------と…---------------")
+except FileNotFoundError:
+    print(f"ファイル '{filename}' が見つからず、神は困惑している…")
