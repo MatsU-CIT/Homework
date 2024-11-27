d73c0afb21bf224ba4d6cae4a52fb8db00170147
Yusuke Matsumoto <yusuke20050315@gmail.com>
2024-11-27 14:39:09 +0900

    お告げ入力用

--- hwcode.py
+++ hwcode.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2024 Yusuke Matsumoto s23c1134bg@s.chibakoudai.jp
+import sys
+
 filename = "godsmsg.txt"
 
 with open(filename, "w", encoding="utf-8") as file:
     print("文章を入力してください。改行も出来ます。（終了するには空行を入力）:")
 
-    while True:
-        line = input()
+    for line in sys.stdin:
+        line = line.strip()
         if line == "":
             break
         file.write(line + "\n")
 
 print(f"入力された内容は '{filename}' に保存されました。")
+
